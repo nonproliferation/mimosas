ed9c1e9486ab2dd41ad31601252137ad21be84a9
Bethany Goldblum <bethany@nuc.berkeley.edu>
2019-09-28 21:29:17 -0700

    updating the documentation

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['config',['config',['../classparameter__parser_1_1Parameters.html#a36a70126f2aaba0ff52da1a6d985df96',1,'parameter_parser::Parameters']]],
   ['config_5ffile',['config_file',['../classparameter__parser_1_1Parameters.html#a9cdc58a06c8ce73fd974587cfd503cda',1,'parameter_parser::Parameters']]],
-  ['construct_5fnetwork',['construct_network',['../namespacefeed__forward__nn.html#a2eaa072d6178d0609ab16b304044b6bb',1,'feed_forward_nn']]],
+  ['construct_5fnetwork',['construct_network',['../namespacefeed__forward__nn.html#adfc12004c0c595fc1463e5bc22c2af5d',1,'feed_forward_nn']]],
   ['create_5fsession_5fdirectory',['create_session_directory',['../namespacemain.html#ae207b2d234b8fa365f3434325d734073',1,'main']]]
 ];
--- docs/search/functions_3.js
+++ docs/search/functions_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['construct_5fnetwork',['construct_network',['../namespacefeed__forward__nn.html#a2eaa072d6178d0609ab16b304044b6bb',1,'feed_forward_nn']]],
+  ['construct_5fnetwork',['construct_network',['../namespacefeed__forward__nn.html#adfc12004c0c595fc1463e5bc22c2af5d',1,'feed_forward_nn']]],
   ['create_5fsession_5fdirectory',['create_session_directory',['../namespacemain.html#ae207b2d234b8fa365f3434325d734073',1,'main']]]
 ];
